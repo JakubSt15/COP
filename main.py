fd9c1ee3638ee47817daa8fc87c1aac7fa15f807
mb51053 <150904257+mb51053@users.noreply.github.com>
2023-11-22 12:08:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import mne
 import pandas as pd
+import pyarrow.feather as feather
 from fuzzywuzzy import process
 import re
 
@@ -21,11 +22,12 @@ def extract_eeg_data(edf_file, channels_of_interest):
 
     return eeg_data, matched_channels
 
-def save_to_csv(eeg_data, channels_of_interest, output_csv):
+def save_to_feather(eeg_data, channels_of_interest, output_feather):
     df = pd.DataFrame(eeg_data.T, columns=channels_of_interest)
 
-    df.to_csv(output_csv, index=False)
-    print(f'Dane zapisane do {output_csv}')
+    feather.write_feather(df, output_feather)
+
+    print(f'Dane zapisane do {output_feather}')
 
 if __name__ == "__main__":
 
@@ -36,56 +38,58 @@ if __name__ == "__main__":
     channels_of_interest = ['Fp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'T3', 'C3', 'Cz', 'C4', 'T4', 'T5', 'P3', 'Pz', 'P4', 'T6', 'O1', 'O2']
     data = []
 
-    # for file_name in files_list:
-    #     file_path = os.path.join("./Baza", file_name)
-    #     eeg_data, matched_channels = extract_eeg_data(file_path, channels_of_interest)
-    #     print(eeg_data)
+    for file_name in files_list:
+        file_path = os.path.join("./Baza", file_name)
+        eeg_data, matched_channels = extract_eeg_data(file_path, channels_of_interest)
+        output_feather = file_name + ".feather"
+        save_to_feather(eeg_data,channels_of_interest,output_feather)
+        print(eeg_data)
 
-    # Ścieżka do katalogu, który chcesz przeszukać
-    files_list = "BadanieDane/"
-
-    # Pętla przeglądająca pliki w katalogu
-    for file_name in os.listdir(files_list):
-        if file_name.endswith(".txt"):
-            # Pełna ścieżka do pliku
-            full_path = os.path.join(files_list, file_name)
-
-            import re
-
-            # Tworzenie pustej listy na dane
-            wyniki = []
-
-            # Otwieranie pliku
-            with open(full_path, 'r') as file:
-                # Pętla przeglądająca linie w pliku
-                for line in file:
-                    # Tutaj możesz przetwarzać zawartość linii, na przykład szukać konkretnego tekstu
-                    if 'File name:' in line:
-                        # Wyrażenie regularne do wyszukiwania elementów zaczynających się od "PN" i kończących się na ".edf"
-                        wyrazenie_regularne = re.compile(r'PN.*\.edf')
-                        # Wyszukaj dopasowanie w linii tekstu
-                        dopasowanie = wyrazenie_regularne.search(line)
-                        if dopasowanie:
-                            wyniki.append(dopasowanie.group())
-
-                    if 'Seizure start time' in line:
-                        # Wyrażenie regularne do wyszukania godziny w formacie HH.MM.SS
-                        wyrazenie_regularne = re.compile(r'(\d{2}.\d{2}.\d{2})')
-                        # Wyszukiwanie dopasowania w linii tekstu
-                        dopasowanie = wyrazenie_regularne.search(line)
-                        if dopasowanie:
-                            wyniki.append(dopasowanie.group(1))
-
-                    if 'Seizure end time:' in line:
-                        # Wyrażenie regularne do wyszukania godziny w formacie HH.MM.SS
-                        wyrazenie_regularne = re.compile(r'(\d{2}.\d{2}.\d{2})')
-                        # Wyszukiwanie dopasowania w linii tekstu
-                        dopasowanie = wyrazenie_regularne.search(line)
-                        if dopasowanie:
-                            wyniki.append(dopasowanie.group(1))
-
-            # Wyświetlanie zawartości tablicy
-            print(wyniki)
+    # # Ścieżka do katalogu, który chcesz przeszukać
+    # files_list = "BadanieDane/"
+    #
+    # # Pętla przeglądająca pliki w katalogu
+    # for file_name in os.listdir(files_list):
+    #     if file_name.endswith(".txt"):
+    #         # Pełna ścieżka do pliku
+    #         full_path = os.path.join(files_list, file_name)
+    #
+    #         import re
+    #
+    #         # Tworzenie pustej listy na dane
+    #         wyniki = []
+    #
+    #         # Otwieranie pliku
+    #         with open(full_path, 'r') as file:
+    #             # Pętla przeglądająca linie w pliku
+    #             for line in file:
+    #                 # Tutaj możesz przetwarzać zawartość linii, na przykład szukać konkretnego tekstu
+    #                 if 'File name:' in line:
+    #                     # Wyrażenie regularne do wyszukiwania elementów zaczynających się od "PN" i kończących się na ".edf"
+    #                     wyrazenie_regularne = re.compile(r'PN.*\.edf')
+    #                     # Wyszukaj dopasowanie w linii tekstu
+    #                     dopasowanie = wyrazenie_regularne.search(line)
+    #                     if dopasowanie:
+    #                         wyniki.append(dopasowanie.group())
+    #
+    #                 if 'Seizure start time' in line:
+    #                     # Wyrażenie regularne do wyszukania godziny w formacie HH.MM.SS
+    #                     wyrazenie_regularne = re.compile(r'(\d{2}.\d{2}.\d{2})')
+    #                     # Wyszukiwanie dopasowania w linii tekstu
+    #                     dopasowanie = wyrazenie_regularne.search(line)
+    #                     if dopasowanie:
+    #                         wyniki.append(dopasowanie.group(1))
+    #
+    #                 if 'Seizure end time:' in line:
+    #                     # Wyrażenie regularne do wyszukania godziny w formacie HH.MM.SS
+    #                     wyrazenie_regularne = re.compile(r'(\d{2}.\d{2}.\d{2})')
+    #                     # Wyszukiwanie dopasowania w linii tekstu
+    #                     dopasowanie = wyrazenie_regularne.search(line)
+    #                     if dopasowanie:
+    #                         wyniki.append(dopasowanie.group(1))
+    #
+    #         # Wyświetlanie zawartości tablicy
+    #         print(wyniki)
 
 
 
