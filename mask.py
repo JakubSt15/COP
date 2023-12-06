0ce26f67b8f0f95e98c1afe44cac5963781e021b
mb51053 <150904257+mb51053@users.noreply.github.com>
2023-12-06 12:26:46 +0100

    Update mask.py

--- mask.py
+++ mask.py
@@ -1,79 +1,63 @@
 import re
 import os
+import csv
+from datetime import datetime, timedelta
 
+with open("badaniaDanetest.csv", "r") as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=';')
 
-katalog = "BadanieDane/"
+    for row in csv_reader:
+        file_name = row['File name']
+        reg_start_str = row['Registration start time']
+        reg_end_str = row['Registration end time']
+        seizure_start_str = row['Seizure start time']
+        seizure_end_str = row['Seizure end time']
 
-files_list = os.listdir(katalog)
-#print(files_list)
+        try:
+            # Convert string times to datetime objects
+            reg_start = datetime.strptime(reg_start_str, "%H.%M.%S")
+            reg_end = datetime.strptime(reg_end_str, "%H.%M.%S")
+            seizure_start = datetime.strptime(seizure_start_str, "%H.%M.%S")
+            seizure_end = datetime.strptime(seizure_end_str, "%H.%M.%S")
 
+        except ValueError as e:
+            print(f"Error processing file {file_name}: {e}")
+            print(f"  Registration Start Time: {reg_start_str}")
+            print(f"  Registration End Time: {reg_end_str}")
+            print(f"  Seizure Start Time: {seizure_start_str}")
+            print(f"  Seizure End Time: {seizure_end_str}")
+            continue
 
-for file_name in files_list:
-    #file_path = os.path.join("./Baza", file_name)
-    with open("./BadanieDane/" + file_name, "r") as file:
-        data = file.read()
+        # Adjust times that cross midnight
+        if reg_start > reg_end:
+            reg_end += timedelta(days=1)
+        if seizure_start > seizure_end:
+            seizure_end += timedelta(days=1)
 
-    pattern = re.compile(r"""
-         Seizure\sn\s\d+\s
-         File\sname:\s\w+-(\d+)\.edf\s
-         Registration\sstart\stime:\s(\d+\.\d+\.\d+)\s
-         Registration\send\stime:\s(\d+\.\d+\.\d+)\s
-         Seizure\sstart\stime:\s(\d+\.\d+\.\d+)\s
-         Seizure\send\stime:\s(\d+\.\d+\.\d+)
-        """, re.VERBOSE)
+        print(f"File Name: {file_name}")
+        print(f"  Registration Start Time: {reg_start.strftime('%H:%M:%S')}")
+        print(f"  Registration End Time: {reg_end.strftime('%H:%M:%S')}")
+        print(f"  Seizure Start Time: {seizure_start.strftime('%H:%M:%S')}")
+        print(f"  Seizure End Time: {seizure_end.strftime('%H:%M:%S')}")
 
-    matches = pattern.findall(data)
+        # Calculate time in seconds
+        reg_start_time = int((reg_start - datetime.min).total_seconds())
+        reg_end_time = int((reg_end - datetime.min).total_seconds())
+        seizure_start_time = int((seizure_start - datetime.min).total_seconds())
+        seizure_end_time = min(int((seizure_end - datetime.min).total_seconds()), reg_end_time)
 
-    seizure_data = []
+        # Calculate array indices
+        seizure_array_start = max(0, seizure_start_time - reg_start_time)
+        seizure_array_end = min(reg_end_time - reg_start_time, seizure_end_time - reg_start_time)
 
-    print('matches ',matches)
-    print('matches len ',len(matches))
-
-
-    for match in matches:
-        seizure_number, reg_start, reg_end, seizure_start, seizure_end = match
-        print(f"Seizure n {seizure_number}:")
-        print(f"  Registration Start Time: {reg_start}")
-        print(f"  Registration End Time: {reg_end}")
-        print(f"  Seizure Start Time: {seizure_start}")
-        print(f"  Seizure End Time: {seizure_end}")
-
-        # Calculate time for each seizure
-        reg_start_time = sum(int(x) * 60 ** i for i, x in enumerate(reversed(reg_start.split('.'))))
-        reg_end_time = sum(int(x) * 60 ** i for i, x in enumerate(reversed(reg_end.split('.'))))
-        seizure_start_time = sum(int(x) * 60 ** i for i, x in enumerate(reversed(seizure_start.split('.'))))
-        #seizure_end_time = sum(int(x) * 60 ** i for i, x in enumerate(reversed(seizure_end.split('.'))))
-        seizure_end_time = min(sum(int(x) * 60 ** i for i, x in enumerate(reversed(seizure_end.split('.')))), reg_end_time)
-
+        # Create and populate seizure_array
         seizure_array = [0] * (reg_end_time - reg_start_time + 1)
-
-        for i in range(seizure_start_time - reg_start_time, seizure_end_time - reg_start_time + 1):
+        for i in range(seizure_array_start, seizure_array_end + 1):
             seizure_array[i] = 1
 
-        seizure_data.append((seizure_number, seizure_array))
-        print()
-
-        text_file_path = file_name + "_seizure_array_data.txt"
-        print(file_name)
+        text_file_path = f"SeizureMask/{file_name}_array_data.txt"
         with open(text_file_path, "w") as text_file:
-            for seizure_number, seizure_array in seizure_data:
-                text_file.write(f"Seizure n {seizure_number} Array:\n")
-                text_file.write(",".join(map(str, seizure_array)) + "\n\n")
+            text_file.write(f"Array for {file_name}:\n")
+            text_file.write(",".join(map(str, seizure_array)) + "\n\n")
 
-        print(f"\nArray data written to: {text_file_path}")
-
-        for seizure_number, seizure_array in seizure_data:
-            print(f"Seizure n {seizure_number} Array:")
-            print(seizure_array,"\n")
-
-
-#Ten przypadek chyba źle jest zapisany, rejestracja kończy się przed zakończeniem ataku???
-#W gruncie rzeczy nie utworzy z tego tablicy
-
-#Seizure n 3
-#File name: PN00-3.edf
-#Registration start time: 18.15.44
-#Registration end time: 18.57.13
-#Seizure start time: 18.28.29
-#Seizure end time: 19.29.29
-
+        print(f"Array data written to: {text_file_path}\n")
